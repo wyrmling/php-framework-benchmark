b32286f1708c8d5e25aaa33d76c9d84b16d4896b
wyrmling <wyrmling@users.noreply.github.com>
2022-05-30 16:42:52 +0300

    update list

--- list.sh
+++ list.sh
@@ -1,54 +1,18 @@
 #!/bin/sh
 
 list="
-aura-2.0
-#bear-0.10
-bear-1.0
-#cake-3.0
-#cake-3.1
-cake-3.2
-#ci-3.0
-ci-3.1
+#cake-3.2
+#ci-3.1
 #ci-4.0-dev
-cygnite-1.3
-fatfree-3.5
-fuel-1.8
-#ice-1.0
-#ice-1.1    # Can't build on CentOS 6
-kumbia-1.0-dev
 #laravel-4.2
-#laravel-5.0
-#laravel-5.1
-#laravel-5.2
-laravel-5.3
-#lumen-5.0
-lumen-5.1
+#laravel-5.3
+laravel-6.x
+#lumen-5.1
 #no-framework
-nofuss-1.2
-#phalcon-1.3
-phalcon-2.0
-phpixie-3.2
-#radar-1.0-dev  # does not work
-siler-0.6
-#silex-1.2
-#silex-1.3
-silex-2.0
-#slim-2.6
 slim-3.0
-staticphp-0.9
-#symfony-2.5
-#symfony-2.6
 #symfony-2.7
-symfony-3.0
-symfony-4.0
-tipsy-0.10
-#typo3f-2.3 # does not work
-#typo3f-3.0 # Catchable Fatal Error: Argument 1 passed to TYPO3\Flow\Object\ObjectManager::setObjects() must be of the type array, null given
-yii-2.0
-ze-1.0
-#zf-2.4
-#zf-1.12
-#zf-2.4
-#zf-2.5
-zf-3.0
+#symfony-3.0
+#symfony-4.0
+#yii-2.0
+#zf-3.0
 "
